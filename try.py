4216ac4374e51ab841be188ca70de7c4713bbc25
bhaumik <bhaumik.yadav@argusoft.com>
2026-07-28 18:42:58 +0530

    Notes ref from https://docs.python.org/3/tutorial/ and code from tutorial ref https://www.youtube.com/watch?v=K5KVEU3aaeQ

---
+++ try.py
@@ -0,0 +1,81 @@
+# print('Hello World')
+# print('*'*11)
+# x = 1
+
+# students_count = 1000
+# rating = 4.56
+# is_published = True
+# name = "Bhaumik Yadav"
+
+# print(type(students_count))
+# print(type(rating))
+# print(type(is_published))
+# print(type(name))
+
+# course = "Python Programming"
+# print(len(course))
+# print(course[0], course[-1])
+# print(course[0:3])
+# print(course[:3])
+# print(course[3:])
+
+# print("LOL \"Escape \\Character")
+
+# first = "Bhaumik"
+# last = "Yadav"
+# full = f'{first} {last}'
+# print(full)
+
+# course = "   Python programming   "
+
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.lstrip())
+# print(course.rstrip())
+# print(course.find("pro"))
+# print(course.replace("o", "i"))
+# print("pro" in course)
+# print("swift" not in course)
+
+# a = 1 + 3j
+
+# print(type(a))
+
+# print(10+3)
+# print(10-3)
+# print(10*3)
+# print(10/3)
+# print(10//3)
+# print(10%3)
+# print(10**3)
+# print(10+3)
+
+# import math 
+
+# print(round(2.9))
+# print(abs(-2.9))
+# print(math.ceil(2.9))
+
+# temp = 35
+
+# if temp > 30:
+#     print("Hot")
+# else:
+#     print("Not hot")
+
+# message = "Hot" if temp > 30 else "Not Hot"
+
+# print(message)
+
+# high_income = False
+# good_credit = True
+# student = True
+
+# if high_income and good_credit:
+#     print('Eligible')
+# elif good_credit and student:
+#     print("Eligible")
+# else:
+#     print("Ineligible")
