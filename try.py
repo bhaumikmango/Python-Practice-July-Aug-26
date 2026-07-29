086158977cbcb2449933ffd4e10786432375f178
bhaumik <bhaumik.yadav@argusoft.com>
2026-07-29 12:40:13 +0530

    Code from tutorial ref https://www.youtube.com/watch?v=K5KVEU3aaeQ

--- try.py
+++ try.py
@@ -79,3 +79,136 @@
 #     print("Eligible")
 # else:
 #     print("Ineligible")
+
+
+# # RULE - Age should be bw 18 and 65
+
+# age = 22
+# if age >=18 and age < 65:
+#     print('Eligible')
+# if 18 <= age < 65:
+#     print('Eligible') # Cleaner code
+
+
+# # Strings compare on unicode values 
+
+# if "bag" > "apple":
+#     print('Yes1')
+# if "bag" > "bah":
+#     print('Yes2')
+# if "bag" > "Bag":
+#     print('Yes3')
+# if "bag" > "bags":
+#     print('Yes4')
+
+# #Loops
+
+# for number in range(3):
+#     print("Sending Message, Attempt:", number + 1, "Index:", number)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+
+# # break statement, else with for loop
+
+# successful = False
+
+# for number in range(1, 4):
+#     print("Attempt:", number)
+#     if successful:
+#         print('Successful')
+#         break
+# else:
+#     print("Attempted 3 times and failed")
+
+
+# # Nested Loops
+
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(f"({i}, {j})")
+
+
+# # Iterables
+
+# print(type(5))
+# print(type(range(5))) #iterable complex data type
+
+# for x in "Python":
+#     print(x)
+
+# for x in [1, 2, 3, 4]:
+#     print(x)
+
+
+# # While Loops
+
+# i = 0
+
+# while i < 5:
+#     print(i)
+#     i+=1
+
+# command = ""
+
+# while command.lower() != "exit":
+#     command = str(input(">"))
+#     print("ECHO", command)
+
+# #Alt for above loop
+# while True:
+#     command = str(input('>'))
+#     print("ECHO", command)
+#     if command.lower() in ["exit", "esc"]:
+#         break
+
+
+# # Program to display even numbers bw 1, 10 using % instead of step in range method
+
+# for i in range(1, 10):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# # UDF - User Defined Functions
+
+# import datetime
+
+# def greet():
+#     a = int(datetime.datetime.now().strftime("%H"))
+#     if 5 <= a < 12:
+#         print("Good Morning")
+#     elif 12 <= a < 16:
+#         print("Good Afternoon")
+#     elif 16 <= a < 20:
+#         print("Good Evening")
+#     else:
+#         print("Good Night")
+
+
+# greet()
+
+
+# # Arguments in functions
+
+# def laugh(is_happy = True): # These are the parameters of a function
+#     return "LOL" if is_happy else "Pokerface"
+
+# print(laugh(False)) # The values which we provide for respected parameters are the arguments
+
+
+# 2 Types of functions 
+# 1 - Perform a task
+# 2 - Return a value
+
+# # * args variable number of arguments
+
+# def mul(*args):
+#     out = 1
+#     for i in args:
+#         out *= i
+#     return out
+
+# print(mul(2, 3))
+# print(mul(2, 3, 4, 5, 6))
