3f9578a25bd2c69c4d9af78da42db9c9eb7ee946
bhaumik <bhaumik.yadav@argusoft.com>
2026-08-05 18:42:34 +0530

    Practice Reference https://pynative.com/intermediate-python-exercises/

--- try2.py
+++ try2.py
@@ -110,3 +110,102 @@
 # o = [i for i in o if i[0].lower() in ['a','e','i','o','u'] and len(i) > 5]
 
 # print(o)
+
+
+# # sequence integrity
+# # Given Input: [1, 2, 2, 3, 1, 4, 2]
+
+# # Expected Output: [1, 2, 3, 4]
+
+# h = [1, 2, 2, 3, 1, 4, 2]
+
+# f = []
+
+# for i in h:
+#     if i not in f:
+#         f.append(i)
+
+# print(f)
+
+
+# # Given Input: List: [1, 2, 3, 4, 5], Shift: 2, Direction: 'right'
+
+# # Expected Output: [4, 5, 1, 2, 3]
+
+# def circular_shift(inp, shift_count, direction):
+#     if direction.lower() == "right":
+#         a = inp[-(shift_count):]
+#         b = inp[0:-(shift_count)]
+#         out = a + b
+#     elif direction.lower() == "left":
+#         a = inp[shift_count:]
+#         b = inp[0:shift_count]
+#         out = a + b
+#     else:
+#         out = "Return a valid rotatory direction"
+#     return out
+
+# print(circular_shift([1, 2, 3, 4, 5], 2, 'left'))
+
+
+# # Given Input: d1 = {"a": 1, "b": 2}, d2 = {"b": 3, "c": 4}
+
+# # Expected Output: {'a': [1], 'b': [2, 3], 'c': [4]}
+
+# d1 = {"a": 1, "b": 2}
+# d2 = {"b": 3, "c": 4}
+
+# merged = d1.copy()
+
+# for key, value in d2.items():
+#     if key in merged:
+#         merged[key] = [value, merged[key]]
+#     else:
+#         merged[key] = value
+
+# print(merged)
+
+
+# # Given Input: {"Orwell": ["1984", "Animal Farm"], "Huxley": ["Brave New World"]}
+
+# # Expected Output: {'1984': 'Orwell', 'Animal Farm': 'Orwell', 'Brave New World': 'Huxley'}
+
+# d = {"Orwell": ["1984", "Animal Farm"], "Huxley": ["Brave New World"]}
+
+# new = {}
+
+# for key, value in d.items():
+#     for v in value:
+#         new[v] = key
+
+# print(new)
+
+
+# # Given Input: employees = [{"name": "A", "salary": 50}, {"name": "B", "salary": 70}, {"name": "C", "salary": 60}]
+
+# # Expected Output: [{'name': 'B', 'salary': 70}, {'name': 'C', 'salary': 60}, {'name': 'A', 'salary': 50}]
+
+# inp = [{"name": "A", "salary": 50}, {"name": "B", "salary": 70}, {"name": "C", "salary": 60}]
+
+# out = sorted(inp, key=lambda x: x['salary'], reverse=True)
+
+# print(out)
+
+
+# # Given Input: Set A: {1, 2, 3}, Set B: {1, 2, 3, 4, 5} (Find subset, superset, disjoint)
+
+# # Expected Output: Set A is a subset of Set B.
+
+# A = {1, 2, 3}
+# B = {1, 4, 5}
+
+# if A.issubset(B):
+#     print("A is a subset of B")
+# elif B.issubset(A):
+#     print("B is a subset of A")
+# elif A.isdisjoint(B):
+#     print("Both sets are disjoint(Share no elements)")
+# else:
+#     print("Common elements in the sets are", A & B)
+
+
